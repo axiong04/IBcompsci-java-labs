b9fe6c3873394892dd2b94c2f619a6cf6180c187
coidingbat <98608953+coidingbat@users.noreply.github.com>
2022-09-28 12:22:45 -0400

    Add files via upload
    doesn't work lmao

---
+++ syntaxChecker/SyntaxCheckRunner.java
@@ -0,0 +1,29 @@
+ //(c) A+ Computer Science
+//www.apluscompsci.com
+
+//Name -
+
+import java.util.Stack;
+import static java.lang.System.*;
+
+public class SyntaxCheckRunner {
+  public static void main(String[] args) {
+
+    SyntaxChecker bob = new SyntaxChecker("(abc(*def)");
+    System.out.println("(abc(*def) " + bob);
+    SyntaxChecker joe = new SyntaxChecker("[{}]");
+    System.out.println("[{}]" + joe);
+    SyntaxChecker hw = new SyntaxChecker("[");
+    System.out.println("[" + hw);
+    SyntaxChecker how = new SyntaxChecker("[{<()>}]");
+    System.out.println("[{<()>}]" + how);
+    SyntaxChecker fred = new SyntaxChecker("{<html[value=4]*(12)>{$x}}");
+    System.out.println("{<html[value=4]*(12)>{$x}}" + fred);
+    SyntaxChecker a = new SyntaxChecker("[one]<two>{three}(four)");
+    System.out.println("[one]<two>{three}(four)" + a);
+    SyntaxChecker kevin = new SyntaxChecker("car(cdr(a)(b)))");
+    System.out.println("car(cdr(a)(b)))" + kevin);
+    SyntaxChecker clash = new SyntaxChecker("car(cdr{a}(b))");
+    System.out.println("car(cdr(a)(b))" + clash);  
+  }
+}
---
+++ syntaxChecker/SyntaxChecker.java
@@ -0,0 +1,65 @@
+//(c) A+ Computer Science
+//www.apluscompsci.com
+
+//Name -
+
+import java.util.Stack;
+import static java.lang.System.*;
+
+public class SyntaxChecker
+{
+	private String exp;
+	private Stack<Character> symbols;
+
+	public SyntaxChecker()
+	{
+    symbols = new Stack<Character>();
+    exp = "";
+	}
+
+	public SyntaxChecker(String s)
+	{
+      symbols = new Stack<Character>();
+    setExpression(s);
+	}
+	
+	public void setExpression(String s)
+	{
+    exp = s;
+	}
+
+	public boolean checkExpression()
+	{
+    char cha; 
+    for(int i = 0; i < exp.length(); i++)
+    {
+      cha=exp.charAt(i);
+      if(cha=='('||cha=='['||cha=='<'||cha=='{') 
+      {
+        symbols.push(cha);
+      }
+      if(cha==')'||cha==']'||cha=='>'||cha=='}') 
+      {
+        if(symbols.size()!=0){
+          if((char)symbols.peek()+1==cha)
+          {
+            symbols.pop();
+          }
+          else
+          {
+            return false;
+          }
+        }
+      }
+    }
+		return true;
+	}
+  public String toString()
+  {
+    if(checkExpression())
+    {
+      return "is incorrect";
+    }
+    return "is correct";
+  }
+}
