09bd73436d436ebf96f46a8bca542b54a5cb8950
coidingbat <98608953+coidingbat@users.noreply.github.com>
2022-09-28 12:24:14 -0400

    Add files via upload
    actually works wtf

---
+++ postfix/PostFix.java
@@ -0,0 +1,56 @@
+// A+ Computer Science
+//www.apluscompsci.com
+
+//Name -
+
+import java.util.Stack;
+import java.util.Scanner;
+import static java.lang.System.*;
+import java.lang.Character;
+import java.util.Arrays;
+public class PostFix
+{
+	public double calc(String expression)
+	{
+    Stack<Double> stack = new Stack<>();
+    for(int i = 0; i < expression.length(); i++){
+      
+    if(Character.isDigit(expression.charAt(i)))
+    {
+      int num = expression.charAt(i)-48;
+
+
+      stack.push((double)num);
+
+    }
+      else if (Character.isDigit(expression.charAt(i))!=true)
+    {
+      if(expression.charAt(i)=='+')
+      {
+        double sum = stack.pop() + stack.pop();
+        stack.push(sum);
+      }
+      if(expression.charAt(i)=='-')
+      {
+        double number1 = stack.pop();
+        double number2 = stack.pop();
+        double difference = number2 - number1;
+        stack.push(difference);
+      }
+      if(expression.charAt(i)=='*')
+      {
+        double product = stack.pop() * stack.pop();
+        stack.push(product);
+      }
+      if(expression.charAt(i)=='/')
+      {
+        double num1 = stack.pop();
+        double num2 = stack.pop();
+        double dividend = num2 / num1;
+        stack.push(dividend);
+      }
+    }
+    }
+		return stack.pop();
+	}
+}
---
+++ postfix/PostFixRunner.java
@@ -0,0 +1,29 @@
+// A+ Computer Science  -  www.apluscompsci.com
+//Name -
+//Date -
+//Class -
+//Lab  -
+
+import java.util.Stack;
+import java.util.Scanner;
+import static java.lang.System.*;
+
+public class PostFixRunner
+{
+	public static void main ( String[] args )
+	{
+		PostFix test = new PostFix();
+		System.out.println("2 7 + 1 2 + + = "+test.calc("2 7 + 1 2 + +"));
+
+		System.out.println("1 2 3 4 + + + = "+test.calc("1 2 3 4 + + +"));
+
+    System.out.println("9 3 * 8 / 4 + = "+test.calc("9 3 * 8 / 4 +"));
+
+		System.out.println("3 3 + 7 * 9 2 / + = "+test.calc("3 3 + 7 * 9 2 / +"));
+
+		System.out.println("9 3 / 2 * 7 9 * + 4 - = "+test.calc("9 3 / 2 * 7 9 * + 4 -"));
+		
+		System.out.println("5 5 + 2 * 4 / 9 + = "+test.calc("5 5 + 2 * 4 / 9 +"));
+	
+	}
+}
